7e19ea940d87a4fcc22b1d39876dbf9b79725cbe
Jose Colon Rodriguez <jec.rod@gmail.com>
2020-10-10 18:17:37 -0400

    Adjusted doc comment and README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# shoogah_macros
+This crate is a helper that provides the macros for the `shoogah` crate.
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,4 @@
-//! shoogah_macros is a crate with all sorts of syntactic sugar for Rust. Many of the
-//! items are inspired from the goodness of other languages, especially *Groovy*.
-//! Some operations require an expanded notion of what is *true* and what is *false*.
-//! In these cases, we make use of the `AsBool` trait from the `as_bool` crate.
-//! Any type that implements `AsBool`, will work with the macros in `shoogah`
-//! that require such *truthiness*.
+//! shoogah_macros is a helper that provides the macros for the `shoogah` crate.
 
 mod cxp;
 mod hml;
